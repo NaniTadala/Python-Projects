d73b46f806b7c1e832216c38b058e675bf3f307c
NaniTadala <92290291+NaniTadala@users.noreply.github.com>
2022-06-29 16:21:03 +0530

    Add files via upload

---
+++ Shuffle the list.py
@@ -0,0 +1,33 @@
+from random import shuffle
+
+def shuffle_list(mylist):
+    '''
+    shuffles my list
+    '''
+    shuffle(mylist)
+    return mylist
+
+def players_guess():
+    guess = ''
+    while guess not in ['0','1','2']:
+        guess = input("Pick a number: 0, 1, or 2:  ")
+    return int(guess)
+
+def check_guess(mylist,guess):
+    if mylist[guess] == 'O':
+        print('Correct Guess!')
+    else:
+        print('Wrong! Better luck next time')
+        print(mylist)
+
+# Initial List
+mylist = [' ','O',' ']
+
+# Shuffle It
+mixedup_list = shuffle_list(mylist)
+
+# Get User's Guess
+guess = players_guess()
+
+# Check User's Guess
+check_guess(mixedup_list,guess)
